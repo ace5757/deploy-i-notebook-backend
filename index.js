52c5e6846765b70f84a7b5bf2ff31d18c6582645
ace5757 <kiliongarrix@gmail.com>
2024-07-24 15:35:33 +0530

    fifth

--- index.js
+++ index.js
@@ -6,9 +6,27 @@ connectToMongo()
 const app = express()
 app.use(cors())        //used to establish a secure connection bw frontend and expresss
 const port = process.env.PORT || 3003
-app.use(cors({
-  origin: 'https://i-notebook-frontend-nine.vercel.app'
-}));
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://i-notebook-frontend-nine.vercel.app"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
+
+  next();
+});
+
 
 app.use(express.json())
 app.use('/v1/auth', require('./routes/auth'))
