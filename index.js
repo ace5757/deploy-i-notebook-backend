dd414627514debe838cd8ffdfcb0a2e9b9e6cfda
ace5757 <kiliongarrix@gmail.com>
2024-07-24 16:02:39 +0530

    seventh

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const connectToMongo = require('./db')
 connectToMongo()
 const app = express()
 app.use(cors())        //used to establish a secure connection bw frontend and expresss
-const port = process.env.PORT || 3003
+const port = 3003
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
